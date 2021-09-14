e1e25a86533860a008d1b33698dbabf504898792
adan <adanalin113@gmail.com>
2021-09-14 19:20:00 -0400

    commit tree 735ff53b94664fc14276652cfaf7c53475b7e76a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (arry,str){
+     return arry.filter(name => name.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(arry,str){
+    return arry.filter(names => names.startsWith(str));
+}
+
+function matchName(arry,str){
+    return arry.filter(obj => obj.name === str);
+}
